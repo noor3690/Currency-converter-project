92b9c7b4914316d9d1d24580b05b919cd86de058
noorasif17 <162204402+noorasif17@users.noreply.github.com>
2024-04-19 04:32:56 -0600

    currency-converter

--- conv.js
+++ conv.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 const currency = {
@@ -6,28 +7,23 @@ const currency = {
     GBP: 0.76,
     INR: 74.57,
     PKR: 280
-    // RIYAL :
-    // TAKA :
-    // YUAN: 
-    // LIRA :
-    // AFGHANI :
 };
 let user_answer = await inquirer.prompt([
     {
         name: 'from',
-        message: 'Enter From Currency',
+        message: chalk.yellow('Enter From Currency'),
         type: 'list',
         choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
     },
     {
         name: 'to',
-        message: 'Enter To Currency',
+        message: chalk.yellow('Enter To Currency'),
         type: 'list',
         choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
     },
     {
         name: 'amount',
-        message: 'Enter Your Amount',
+        message: chalk.yellow('Enter Your Amount'),
         type: 'number'
     },
 ]);
@@ -38,11 +34,3 @@ let baseAmount = amount / fromAmount;
 let convertedamount = baseAmount * toAmount;
 // console.log(` ${fromAmount} \n ${toAmount} \n ${amount} `);
 console.log(chalk.green(convertedamount));
-let exitContinue = await inquirer.prompt({
-    name: 'exist',
-    message: 'Do you want to exist or continue',
-    type: 'list',
-    choices: ['Continue', 'Exist']
-});
-while (exitContinue.exist != 'Exit')
-    ;
